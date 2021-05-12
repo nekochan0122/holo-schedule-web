a592b148277e723152421f05167c3998e71c2315
NekoChanTW <aaron74327@gmail.com>
2021-05-12 04:16:07 -0700

    add repo link

--- src/components/Main/About/index.jsx
+++ src/components/Main/About/index.jsx
@@ -9,9 +9,11 @@ const About = _ => {
 
       <Typography align='center' variant='subtitle2' color='textPrimary' >
         Powered by
-        <Link href='https://github.com/YunzheZJU/non-stop-story' target='_blank' rel='noreferrer'> Non-stop-story</Link>
+        <Link href='https://github.com/YunzheZJU/non-stop-story' target='_blank' rel='noreferrer'>Non-stop-story</Link>
         <br />
         Discord : NekoChan#2851
+        <br />
+        <Link href='https://github.com/NekoChanTaiwan/holo-schedule-web' target='_blank' rel='noreferrer'>NekoChanTaiwan/holo-schedule-web</Link>
       </Typography>
     </>
   )
