932bd69a8e5a6e2bd582617c31ea8f98effeda4f
NekoChanTW <aaron74327@gmail.com>
2021-05-16 09:16:50 -0700

    Update request.js

--- src/request.js
+++ src/request.js
@@ -4,26 +4,31 @@ const holoDev = axios.create({
   baseURL: 'https://holo.dev/api/v1/'
 })
 
-const getCurrent = async () => {
+const getCurrent = async _ => {
   const res = await holoDev.get('lives/current')
   return res.data.lives
 }
 
 
-const getScheduled = async () => {
+const getScheduled = async _ => {
   const res = await holoDev.get('lives/scheduled')
   return res.data.lives
 }
 
-const getMembers = async () => {
+const getEnded = async _ => {
+  const res = await holoDev.get('lives/ended')
+  return res.data.lives
+}
+
+const getMembers = async _ => {
   const res = await holoDev.get('members')
   return res.data
 }
 
-const getChannels = async () => {
+const getChannels = async _ => {
   const res1 = await holoDev.get('channels?limit=100&page=1')
   const res2 = await holoDev.get('channels?limit=100&page=2')
   return [...res1.data.channels, ...res2.data.channels]
 }
 
-export { getCurrent, getScheduled, getMembers, getChannels }
+export { getCurrent, getScheduled, getEnded, getMembers, getChannels }
